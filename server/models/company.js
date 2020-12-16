34386d88b5e870f34fa93c1f69c2afa115c5e9ab
Gregor Vand <gregor@pixelcabin.io>
2020-12-16 12:34:56 +0800

    Add Company model and migration

---
+++ server/models/company.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const Company = sequelize.define('Company', {
+    nameIdentifier: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    emailIdentifier: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    orderPrefix: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    orderSuffix: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  Company.associate = (models) => {
+    Company.hasMany(models.Order, {
+      foreignKey: 'companyId',
+      as: 'orders',
+    });
+  };
+  return Todo;
+};
