246e078d7d750b4c87bb6bcc8c6c500b4ccada3a
Gregor Vand <gregor@pixelcabin.io>
2020-12-17 15:01:05 +0800

    working regex to find order numbers starting with #. Needs work to variablize prefix in regex.

--- server/modules/emailHelpers.js
+++ server/modules/emailHelpers.js
@@ -1,6 +1,17 @@
 module.exports = {
   returnOrderNumber: function (subject) {
-    console.log(`${subject} was the order number`);
+    // TODO: get prefix from Company record first
+    // const prefix = "\#";
+    // const regex = "\#(?=\w*)\w+"
+    try {
+      console.log(subject);
+      const found = subject.match(/\#(?=\w*)\w+/g);
+      console.log(`${found[0]} was the order number`);
+      console.log(typeof(subject));
+    } catch(err) {
+      console.error(err, 'probably no regex match');
+      return 'No match to the defined prefix'
+    }
   },
 
   parseSubjectForOrder: function(req) {
