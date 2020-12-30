f416690d514415259aba8ee4fafc1a6af1ff64f5
Gregor Vand <gregor@pixelcabin.io>
2020-12-30 09:16:43 +0800

    looks up DB for user, returns with error if user already found

--- server/modules/registerForm.js
+++ server/modules/registerForm.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const User = require('../models').User;
 
 async function registerForm (req, res) {
   let { firstName, lastName, email, password, password2 } = req.body;
@@ -25,12 +26,37 @@ async function registerForm (req, res) {
     errors.push({ message: "Passwords do not match" });
   }
 
+  // errors or not
   if (errors.length > 0) {
     res.render("register", { errors, firstName, lastName, email, password, password2 });
   } else {
     let hashedPassword = await bcrypt.hash(password, 15);
     console.log('hashed happened..', hashedPassword);
+
+    // wait on getting result from DB lookup with entered email
+    const currentUser = await checkDbForUser(email);
+
+    if (currentUser) {
+      errors.push({ message: "Already found a user with that email" });
+      res.render("register", { errors, firstName, lastName, email, password, password2 });
+      console.log(`already found a user called ${currentUser.firstName} ${currentUser.lastName}`)
+    } else {
+      console.log('new user!');
+    }
   }
+}
+
+
+// internal functions
+async function checkDbForUser (registerEmail) {
+    return User
+      .findOne({
+        where: [{
+          email: registerEmail
+        }],
+      })
+      .then((foundUser) => foundUser)
+      .catch((error) => console.error(error));
 }
 
 module.exports.registerForm = registerForm;
