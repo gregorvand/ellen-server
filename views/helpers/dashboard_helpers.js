486f1635899bb1d161188cc70c903ff7dabc9a2c
Gregor Vand <gregor@pixelcabin.io>
2021-01-24 14:06:29 +0800

    Lots of dashboard styling and content, across device sizes

--- views/helpers/dashboard_helpers.js
+++ views/helpers/dashboard_helpers.js
@@ -1,10 +1,12 @@
 const dayjs = require('dayjs');
+const calendar = require('dayjs/plugin/calendar');
+dayjs.extend(calendar)
 
 // next helper: return company name if first instance of that company Id.
 // skip if not the first instance.
 
 const showDate = (date) => {
-  return dayjs(date).format('DD/MM/YYYY');
+  return dayjs(date).calendar();
 }
 
 module.exports.showDate = showDate;
