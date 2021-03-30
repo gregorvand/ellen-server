63f1bd740ba4e44c0e90a40ade590eb1bf8aae8c
Gregor Vand <gregor@pixelcabin.io>
2021-03-30 13:41:42 +0800

    User is added to Klaviyo on signup / changes hasAccount status to true if Profile already found

--- server/services/klaviyo.js
+++ server/services/klaviyo.js
@@ -1,11 +1,12 @@
 const Klaviyo = require('node-klaviyo');
 const env = process.env.NODE_ENV || 'development';
 
-const addSubscribersToList = (req, res) => {
-  const KlaviyoClient = new Klaviyo({
-    privateToken: process.env.KLAVIYO_PRIVATE_KEY,
-  });
+const KlaviyoClient = new Klaviyo({
+  publicToken: 'WD8qqU',
+  privateToken: process.env.KLAVIYO_PRIVATE_KEY
+});
 
+const addSubscribersToList = (req, res) => {
   console.log(req);
   console.log('klaviyo email?', req.body.email);
 
@@ -14,6 +15,7 @@ const addSubscribersToList = (req, res) => {
     profiles: [
         {
           email: req.body.email,
+          signup_location: 'homepage'
         }
     ]
   })
@@ -21,4 +23,18 @@ const addSubscribersToList = (req, res) => {
   .catch(error => res.status(400).send(error));
 }
 
+const identifyUserInternal = (userEmail) => {
+  console.log('klaviyo email?', userEmail);
+
+  KlaviyoClient.public.identify({
+    email: userEmail,
+    properties: {
+      hasAccount: true
+    }
+  })
+  .then(user => console.log(user || 'no user found...'))
+  .catch(error => console.log(error));
+}
+
 module.exports.addSubscribersToList = addSubscribersToList;
+module.exports.identifyUserInternal = identifyUserInternal;
