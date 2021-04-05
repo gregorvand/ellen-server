d4eaeb27c3e82739b55b81dff5fd6e24ca68dad1
Gregor Vand <gregor@pixelcabin.io>
2021-04-05 17:43:30 +0800

    add Points via customerId foreign key

---
+++ server/migrations/8-add-points.js
@@ -0,0 +1,147 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * createTable "Points", deps: [Users]
+ * changeColumn "password" on table "Users"
+ *
+ **/
+
+var info = {
+    "revision": 8,
+    "name": "add-points",
+    "created": "2021-04-05T09:43:14.012Z",
+    "comment": ""
+};
+
+var migrationCommands = function(transaction) {
+    return [{
+            fn: "createTable",
+            params: [
+                "Points",
+                {
+                    "id": {
+                        "type": Sequelize.INTEGER,
+                        "field": "id",
+                        "autoIncrement": true,
+                        "primaryKey": true,
+                        "allowNull": false
+                    },
+                    "pointsValue": {
+                        "type": Sequelize.INTEGER,
+                        "field": "pointsValue",
+                        "allowNull": false
+                    },
+                    "createdAt": {
+                        "type": Sequelize.DATE,
+                        "field": "createdAt",
+                        "allowNull": false
+                    },
+                    "updatedAt": {
+                        "type": Sequelize.DATE,
+                        "field": "updatedAt",
+                        "allowNull": false
+                    },
+                    "customerId": {
+                        "type": Sequelize.INTEGER,
+                        "field": "customerId",
+                        "onUpdate": "CASCADE",
+                        "onDelete": "CASCADE",
+                        "references": {
+                            "model": "Users",
+                            "key": "id"
+                        },
+                        "allowNull": true
+                    }
+                },
+                {
+                    "transaction": transaction
+                }
+            ]
+        },
+        {
+            fn: "changeColumn",
+            params: [
+                "Users",
+                "password",
+                {
+                    "type": Sequelize.STRING(64),
+                    "field": "password",
+                    "allowNull": false,
+                    "is": {}
+                },
+                {
+                    transaction: transaction
+                }
+            ]
+        }
+    ];
+};
+var rollbackCommands = function(transaction) {
+    return [{
+            fn: "dropTable",
+            params: ["Points", {
+                transaction: transaction
+            }]
+        },
+        {
+            fn: "changeColumn",
+            params: [
+                "Users",
+                "password",
+                {
+                    "type": Sequelize.STRING(64),
+                    "field": "password",
+                    "allowNull": false,
+                    "is": {}
+                },
+                {
+                    transaction: transaction
+                }
+            ]
+        }
+    ];
+};
+
+module.exports = {
+    pos: 0,
+    useTransaction: true,
+    execute: function(queryInterface, Sequelize, _commands)
+    {
+        var index = this.pos;
+        function run(transaction) {
+            const commands = _commands(transaction);
+            return new Promise(function(resolve, reject) {
+                function next() {
+                    if (index < commands.length)
+                    {
+                        let command = commands[index];
+                        console.log("[#"+index+"] execute: " + command.fn);
+                        index++;
+                        queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                    }
+                    else
+                        resolve();
+                }
+                next();
+            });
+        }
+        if (this.useTransaction) {
+            return queryInterface.sequelize.transaction(run);
+        } else {
+            return run(null);
+        }
+    },
+    up: function(queryInterface, Sequelize)
+    {
+        return this.execute(queryInterface, Sequelize, migrationCommands);
+    },
+    down: function(queryInterface, Sequelize)
+    {
+        return this.execute(queryInterface, Sequelize, rollbackCommands);
+    },
+    info: info
+};
--- server/models/user.js
+++ server/models/user.js
@@ -34,7 +34,7 @@ module.exports = (sequelize, DataTypes) => {
       as: 'orders',
     });
     User.hasMany(models.Point, {
-      foreignKey: 'pointId',
+      foreignKey: 'customerId',
       as: 'points',
     });
     raw: true
