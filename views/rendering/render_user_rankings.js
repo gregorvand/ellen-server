5608434b0f76b988ce666d1156e7fbf6079d6b67
Gregor Vand <gregor@pixelcabin.io>
2021-04-23 09:21:01 +0800

    show live rankings to Admin view with user details

--- views/rendering/render_user_rankings.js
+++ views/rendering/render_user_rankings.js
@@ -2,13 +2,15 @@ const pointsController = require('../../server/controllers/points');
 // const winnerCalculators = require('../../server/services/winners/winner_calculators');
 
 const renderRankedUsers = function(req, res) {
-  const dailyRankedUsersPromise = pointsController.dailyRankedList(req, res, true)
+  const dailyRankedUsersPromise = pointsController.dailyRankedList(req, res)
   .then(returnedUsers => {
-    rankedUsers = returnedUsers
+    const records = returnedUsers.map(result => result.dataValues);
+    rankedUsers = records;
   })
   .catch((error) => res.status(400).send(error));
   
   Promise.all([dailyRankedUsersPromise]).then(() => {
+    // console.log('render ranked...', rankedUsers);
     res.render("admin/user_rankings", { 
       rankedUserList: rankedUsers
     });
