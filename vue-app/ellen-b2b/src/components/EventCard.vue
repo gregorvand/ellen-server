caf7bbb96a38056dbbaeb1ade1c4d06b67806756
Gregor Vand <gregor@pixelcabin.io>
2021-05-25 16:03:49 +0800

    Add child components

--- vue-app/ellen-b2b/src/components/EventCard.vue
+++ vue-app/ellen-b2b/src/components/EventCard.vue
@@ -1,24 +1,53 @@
 <template>
-  <div>
-    <h1>{{ title }} | <span>event</span></h1>
-  </div>
+  <router-link
+    class="event-link"
+    :to="{ name: 'event-show', params: { id: '1' } }"
+  >
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendees.length }} attending</span>
+    </div>
+  </router-link>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      title: 'Park Cleanup',
+      event: {
+        id: 1,
+        title: 'Park Cleanup',
+        date: '19th November 2021',
+        time: '3.00pm',
+        attendees: [
+          { id: 1, name: 'GV' },
+          { id: 2, name: 'JH' },
+        ],
+      },
     }
   },
 }
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  color: green;
-  span {
-    color: red;
-  }
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
 }
 </style>
--- vue-app/ellen-b2b/src/views/EventList.vue
+++ vue-app/ellen-b2b/src/views/EventList.vue
@@ -2,10 +2,7 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard />
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }">
-      Show Event #1
-    </router-link>
-    |
+    <BaseIcon />
   </div>
 </template>
 
