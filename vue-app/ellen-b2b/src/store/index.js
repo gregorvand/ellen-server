d901d26144275b485fc4ed385c9e93a5222db7eb
Gregor Vand <gregor@pixelcabin.io>
2021-05-31 12:30:17 +0800

    VueX stuff - adds mock server, first Vuex data and actions with corresponding methods in Event create

--- vue-app/ellen-b2b/src/store/index.js
+++ vue-app/ellen-b2b/src/store/index.js
@@ -1,11 +1,52 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community',
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...' },
+      { id: 2, title: '...', organizer: '...' },
+      { id: 3, title: '...', organizer: '...' },
+      { id: 4, title: '...', organizer: '...' },
+    ],
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    },
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    },
+  },
   modules: {},
-});
+  getters: {
+    catLength: (state) => {
+      return state.categories.length
+    },
+    activeTodosCount: (state, getters) => {
+      // getters within getters
+      return state.todos.length - getters.doneTodos.length
+    },
+    getEventById: (state) => (id) => {
+      // dynamic getters
+      return state.events.find((event) => event.id === id)
+    },
+  },
+})
