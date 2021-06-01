a11279e740dde2f5230811c83073f5f3f7832d64
Gregor Vand <gregor@pixelcabin.io>
2021-06-01 14:18:23 +0800

    file type

--- vue-app/ellen-b2b/src/store/store.js
+++ vue-app/ellen-b2b/src/store/store.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import EventService from '@/services/EventService.js'
 import * as user from '@/store/modules/user.js'
-import * as event from '@/store/modules/event.js'
+import * as event from '@/store/modules/events.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
