aaa340cdd7c25767f8741b2f037c2d8315b49542
Gregor Vand <gregor@pixelcabin.io>
2021-06-06 14:24:47 +0800

    Rejig nav to use AppNav component

--- vue-app/ellen-b2b/src/App.vue
+++ vue-app/ellen-b2b/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <NavBar />
+    <AppNav />
     <NotificationContainer />
     <router-view :key="$route.fullPath" />
   </div>
 </template>
 
 <script>
-import NavBar from '@/components/NavBar.vue'
+import AppNav from '@/components/AppNav.vue'
 import NotificationContainer from '@/components/NotificationContainer.vue'
 
 export default {
   components: {
-    NavBar,
+    AppNav,
     NotificationContainer,
   },
 }
