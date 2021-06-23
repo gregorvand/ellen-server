a53857fe7cd146052614141f19f9029facb28f74
Gregor Vand <gregor@pixelcabin.io>
2021-06-23 09:13:15 +0800

    adds basic Earnings view and store

---
+++ vue-app/ellen-b2b/src/store/modules/earnings.js
@@ -0,0 +1,23 @@
+export const state = {
+  liveEarnings: [],
+}
+
+export const getters = {
+  getterValue: (state) => {
+    return state.value
+  },
+}
+
+export const mutations = {
+  PUSH(state, report) {
+    state.liveEarnings.push({
+      ...report,
+    })
+  },
+}
+
+export const actions = {
+  addReportToEarnings({ commit }, report) {
+    commit('PUSH', report)
+  },
+}
---
+++ vue-app/ellen-b2b/src/views/EarningsFeed.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>its Earnings time..</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
