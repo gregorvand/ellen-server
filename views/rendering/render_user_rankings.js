951436aaeb3ecd71a0d74a03e849752741441aeb
Gregor Vand <gregor@pixelcabin.io>
2021-07-09 08:18:47 +0800

    format / remove log

--- views/rendering/render_user_rankings.js
+++ views/rendering/render_user_rankings.js
@@ -1,19 +1,19 @@
-const pointsController = require('../../server/controllers/points');
+const pointsController = require('../../server/controllers/points')
 
-const renderRankedUsers = function(req, res) {
-  const dailyRankedUsersPromise = pointsController.dailyRankedList()
-  .then(returnedUsers => {
-    const records = returnedUsers.map(result => result.dataValues);
-    rankedUsers = records;
-  })
-  .catch((error) => res.status(400).send(error));
-  
+const renderRankedUsers = function (req, res) {
+  const dailyRankedUsersPromise = pointsController
+    .dailyRankedList()
+    .then((returnedUsers) => {
+      const records = returnedUsers.map((result) => result.dataValues)
+      rankedUsers = records
+    })
+    .catch((error) => res.status(400).send(error))
+
   Promise.all([dailyRankedUsersPromise]).then(() => {
-    // console.log('render ranked...', rankedUsers);
-    res.render("admin/user_rankings", { 
-      rankedUserList: rankedUsers
-    });
+    res.render('admin/user_rankings', {
+      rankedUserList: rankedUsers,
+    })
   })
 }
 
-module.exports.renderRankedUsers = renderRankedUsers;
+module.exports.renderRankedUsers = renderRankedUsers
