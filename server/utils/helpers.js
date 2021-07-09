1ad876a11c38161045d176fbeb37e0a805c1175a
Gregor Vand <gregor@pixelcabin.io>
2021-07-09 08:16:36 +0800

    remove logs

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,7 +1,7 @@
 // general helper functions
 
 module.exports = {
-   /**
+  /**
    * Filters an array of objects according to the supplied key name
    * @param {Array} array : array to filter
    * @param {String} keyName : key to filter on
@@ -12,9 +12,7 @@ module.exports = {
     if (keyName == '') {
       console.error('no key supplied to filter on')
     } else {
-      console.log(keyName)
       let keys = array.map((object) => object[keyName])
-      console.log(keys)
       const filtered = array.filter(
         (item, index) => !keys.includes(item[keyName], index + 1)
       )
