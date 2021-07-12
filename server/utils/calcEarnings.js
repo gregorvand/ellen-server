a3c0790be931ee7a3af439ac7bbfce6eb350fc56
Gregor Vand <gregor@pixelcabin.io>
2021-07-12 13:34:24 +0800

    adds helper which returns percentage change in earnings

--- server/utils/calcEarnings.js
+++ server/utils/calcEarnings.js
@@ -6,10 +6,37 @@ module.exports = {
       where: {
         ticker: ticker,
       },
-      order: [['createdAt', 'DESC']],
+      order: [['filingDate', 'DESC']],
       limit: 4,
     })
 
     return earnings
+  },
+
+  /**
+   * Pass a set of earnings to get the difference between the first and second values
+   * @param {Array} earnings : array of Seqelize objects that will have 'revenue' column
+   *
+   * @returns {Integer} Positive or Negative integer of percentage change. e.g. -5 [down 5%], 10 [up 10%], etc
+   */
+  calculateDifferenceOfEarnings(earnings) {
+    const figureOne = earnings[0]?.dataValues.revenue
+    const figureTwo = earnings[1]?.dataValues.revenue
+
+    function percIncrease(a, b) {
+      let percent
+      if (b !== 0) {
+        if (a !== 0) {
+          percent = ((b - a) / a) * 100
+        } else {
+          percent = b * 100
+        }
+      } else {
+        percent = -a * 100
+      }
+      return Math.floor(percent)
+    }
+
+    return percIncrease(figureTwo, figureOne)
   },
 }
