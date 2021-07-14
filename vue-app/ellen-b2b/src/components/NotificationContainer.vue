16c0f0f0c234fc6ef1c01b24d007ed105f3fc8ec
Gregor Vand <gregor@pixelcabin.io>
2021-07-14 09:21:43 +0800

    Modifies API button component to be more flexible. Adds email action for admin

--- vue-app/ellen-b2b/src/components/NotificationContainer.vue
+++ vue-app/ellen-b2b/src/components/NotificationContainer.vue
@@ -18,11 +18,10 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .notification-container {
   position: fixed;
   bottom: 0;
-  background-color: red;
   width: 100%;
   left: 0;
   color: white;
