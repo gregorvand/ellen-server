36d4483282a9eedc4441e7e1db704df7a2b49965
Gregor Vand <gregor@pixelcabin.io>
2021-07-15 11:55:09 +0800

    fix DB type for EarningCalendar

--- server/models/earning_calendar.js
+++ server/models/earning_calendar.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const EarningCalendar = sequelize.define('EarningCalendar', {
     ticker: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
+      type: DataTypes.STRING,
       allowNull: false,
     },
     date: {
