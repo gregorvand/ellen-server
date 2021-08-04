f4bf40efd46058e826e11e3226e89c2df9488d89
Gregor Vand <gregor@pixelcabin.io>
2021-08-04 13:55:23 +0800

    better handling of Edison data where order number may not be valid or container letters

--- server/controllers/ordersByEdison.js
+++ server/controllers/ordersByEdison.js
@@ -2,29 +2,34 @@ const Order = require('../models').Order
 
 const insertEdisonRow = async function (companyId, ...edisonRow) {
   const edisonData = edisonRow[0]
-
-  const isAnOrder = await Order.count({
-    where: {
-      orderNumber: BigInt(edisonData.order_number),
-    },
-  })
-
-  console.log('is already added?', isAnOrder)
-
-  if (isAnOrder < 1) {
-    await Order.create({
-      orderNumber: BigInt(edisonData.order_number),
-      fromEmail: edisonData.from_domain,
-      customerEmail: edisonData.user_id,
-      plainContent: 'not available',
-      // totalValue:
-      //   edisonData.order_total_amount == '' ? 0 : edisonData.order_total_amount, // run decimal migration
-      totalValue: 0,
-      companyId: companyId,
-      orderDate: edisonData.email_time,
+  const orderNumberIdentifier = edisonData.order_number
+  if (typeof edisonData.order_number == 'number') {
+    console.log(typeof orderNumberIdentifier)
+    const isAnOrder = await Order.count({
+      where: {
+        orderNumber: BigInt(orderNumberIdentifier),
+      },
     })
+
+    console.log('is already added?', isAnOrder)
+
+    if (isAnOrder < 1) {
+      await Order.create({
+        orderNumber: BigInt(orderNumberIdentifier),
+        fromEmail: edisonData.from_domain,
+        customerEmail: edisonData.user_id,
+        plainContent: 'not available',
+        // totalValue:
+        //   edisonData.order_total_amount == '' ? 0 : edisonData.order_total_amount, // run decimal migration
+        totalValue: 0,
+        companyId: companyId,
+        orderDate: edisonData.email_time,
+      })
+    } else {
+      console.log('already had', orderNumberIdentifier)
+    }
   } else {
-    console.log('already had', edisonData.order_number)
+    console.log('invalid order number', orderNumberIdentifier)
   }
 }
 
