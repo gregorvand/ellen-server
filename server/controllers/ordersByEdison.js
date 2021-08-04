98538bb2659f2e20733d1f416b85d5ee6cc0b2d4
Gregor Vand <gregor@pixelcabin.io>
2021-08-04 15:05:22 +0800

    better logging for when inserting company data

--- server/controllers/ordersByEdison.js
+++ server/controllers/ordersByEdison.js
@@ -4,16 +4,14 @@ const insertEdisonRow = async function (companyId, ...edisonRow) {
   const edisonData = edisonRow[0]
   const orderNumberIdentifier = edisonData.order_number
   if (typeof edisonData.order_number == 'number') {
-    console.log(typeof orderNumberIdentifier)
     const isAnOrder = await Order.count({
       where: {
         orderNumber: BigInt(orderNumberIdentifier),
       },
     })
 
-    console.log('is already added?', isAnOrder)
-
     if (isAnOrder < 1) {
+      console.log(`inserting ${orderNumberIdentifier}`)
       await Order.create({
         orderNumber: BigInt(orderNumberIdentifier),
         fromEmail: edisonData.from_domain,
