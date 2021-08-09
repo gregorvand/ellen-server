74397a40db35cb2cc2b5cc6d712d22556ad5d7b2
Gregor <gregor@vand.hk>
2021-08-09 09:32:09 +0800

    Cron change (#110)
    * always reset crons. make crons less frequent

--- server/services/earnings/earningsDailyQueue.js
+++ server/services/earnings/earningsDailyQueue.js
@@ -11,7 +11,7 @@ async function initCronAddEarnings() {
   console.log('init cron for getting events every hour')
   addEarningProcessingQueue.add(
     { event: 'activate initEarningsQueues' },
-    { repeat: { cron: '03 2,4,6,10 * * *' } } // 3rd minute of every hour, every day
+    { repeat: { cron: '03 2,10 * * *' } } // 3rd minute of 2nd/10th hours
   )
 }
 
@@ -19,7 +19,7 @@ async function initCronAddCalendarEvents() {
   console.log('init cron for getting cal events every hour')
   addCalendarProcessingQueue.add(
     { event: 'activate initCalQueues' },
-    { repeat: { cron: '01 2,4,6,10 * * *' } } // 1st minute of every hour, every day
+    { repeat: { cron: '01 2,10 * * *' } } 
   )
 }
 
@@ -27,9 +27,12 @@ async function initCronEmailPublicResults() {
   console.log('init cron for sending emails for public companies')
   addEmailProcessingQueue.add(
     { event: 'activate initEmailQueues' },
-    { repeat: { cron: '45 2,4,6,10 * * *' } } // 1st minute of every hour, every day
+    { repeat: { cron: '45 2,10 * * *' } }
   )
 }
+
+// TODO
+// Add cron job to remove dead earning events from DB that are older than X days
 
 // test: this function should otherwise be started and not stopped
 module.exports = {
