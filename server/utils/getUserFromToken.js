6974e7f3d076e4fc69b7abb9972b9b28967670e9
Gregor Vand <gregor@pixelcabin.io>
2021-09-01 14:57:14 +0800

    remove lof

--- server/utils/getUserFromToken.js
+++ server/utils/getUserFromToken.js
@@ -3,7 +3,6 @@ const User = require('../models').User
 
 module.exports = {
   async currentUser(token) {
-    console.log('token...', token)
     const decoded = jwt.verify(token, process.env.USER_AUTH_SECRET)
     const user = await User.findOne({
       where: { email: decoded.user.email },
