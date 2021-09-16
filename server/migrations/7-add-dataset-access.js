29ddd8a44d31433da3dc38638c79dfea3279c780
Gregor Vand <gregor@pixelcabin.io>
2021-09-16 11:42:54 +0800

    DatasetAccess add model migration

---
+++ server/migrations/7-add-dataset-access.js
@@ -0,0 +1,152 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * createTable "DatasetAccesses", deps: [Users]
+ * changeColumn "password" on table "Users"
+ *
+ **/
+
+var info = {
+    "revision": 7,
+    "name": "add-dataset-access",
+    "created": "2021-09-16T02:46:13.170Z",
+    "comment": ""
+};
+
+var migrationCommands = function(transaction) {
+    return [{
+            fn: "createTable",
+            params: [
+                "DatasetAccesses",
+                {
+                    "id": {
+                        "type": Sequelize.INTEGER,
+                        "field": "id",
+                        "autoIncrement": true,
+                        "primaryKey": true,
+                        "allowNull": false
+                    },
+                    "datasetId": {
+                        "type": Sequelize.INTEGER,
+                        "field": "datasetId",
+                        "allowNull": false
+                    },
+                    "expiry": {
+                        "type": Sequelize.DATE,
+                        "field": "expiry",
+                        "allowNull": false
+                    },
+                    "createdAt": {
+                        "type": Sequelize.DATE,
+                        "field": "createdAt",
+                        "allowNull": false
+                    },
+                    "updatedAt": {
+                        "type": Sequelize.DATE,
+                        "field": "updatedAt",
+                        "allowNull": false
+                    },
+                    "customerId": {
+                        "type": Sequelize.INTEGER,
+                        "field": "customerId",
+                        "onUpdate": "CASCADE",
+                        "onDelete": "CASCADE",
+                        "references": {
+                            "model": "Users",
+                            "key": "id"
+                        },
+                        "allowNull": true
+                    }
+                },
+                {
+                    "transaction": transaction
+                }
+            ]
+        },
+        {
+            fn: "changeColumn",
+            params: [
+                "Users",
+                "password",
+                {
+                    "type": Sequelize.STRING(64),
+                    "field": "password",
+                    "allowNull": false,
+                    "is": {}
+                },
+                {
+                    transaction: transaction
+                }
+            ]
+        }
+    ];
+};
+var rollbackCommands = function(transaction) {
+    return [{
+            fn: "dropTable",
+            params: ["DatasetAccesses", {
+                transaction: transaction
+            }]
+        },
+        {
+            fn: "changeColumn",
+            params: [
+                "Users",
+                "password",
+                {
+                    "type": Sequelize.STRING(64),
+                    "field": "password",
+                    "allowNull": false,
+                    "is": {}
+                },
+                {
+                    transaction: transaction
+                }
+            ]
+        }
+    ];
+};
+
+module.exports = {
+    pos: 0,
+    useTransaction: true,
+    execute: function(queryInterface, Sequelize, _commands)
+    {
+        var index = this.pos;
+        function run(transaction) {
+            const commands = _commands(transaction);
+            return new Promise(function(resolve, reject) {
+                function next() {
+                    if (index < commands.length)
+                    {
+                        let command = commands[index];
+                        console.log("[#"+index+"] execute: " + command.fn);
+                        index++;
+                        queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                    }
+                    else
+                        resolve();
+                }
+                next();
+            });
+        }
+        if (this.useTransaction) {
+            return queryInterface.sequelize.transaction(run);
+        } else {
+            return run(null);
+        }
+    },
+    up: function(queryInterface, Sequelize)
+    {
+        return this.execute(queryInterface, Sequelize, migrationCommands);
+    },
+    down: function(queryInterface, Sequelize)
+    {
+        return this.execute(queryInterface, Sequelize, rollbackCommands);
+    },
+    info: info
+};
