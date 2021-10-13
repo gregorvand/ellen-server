711d80e00159e6477998f25223e05b27e69dbea9
Gregor Vand <gregor@pixelcabin.io>
2021-10-13 19:54:21 +0800

    update script before attempt bulk changes

--- db_population/import_csv_orders.js
+++ db_population/import_csv_orders.js
@@ -2,11 +2,12 @@ require('dotenv').config()
 const csv = require('csvtojson')
 const cliProgress = require('cli-progress')
 const ordersControllerEdison = require('../server/controllers/ordersByEdison')
+let throttle = require('lodash.throttle')
 
 const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
 
 // get data
-const csvFilePath = '../edison_data/export_clbcf_013.csv'
+const csvFilePath = '../edison_data/export_ujgly_002.csv'
 const csvFilesPathsArray = [
   '../edison_data/export_clbcf_012.csv',
   '../edison_data/export_clbcf_013.csv',
@@ -34,7 +35,6 @@ async function importAllCSVToOrders(csvFilePath) {
   const data = await convertCSV(csvFilePath)
 
   let fileName = csvFilePath.split('/')
-  console.log(`processing ${fileName[fileName.length - 1]}`)
 
   let barProgress = 1
   bar1.start(data.length, 0)
@@ -49,6 +49,7 @@ async function importAllCSVToOrders(csvFilePath) {
       if (barProgress == data.length + 1) {
         bar1.stop()
         console.log('added', insertCount)
+        console.log(`processed ${fileName[fileName.length - 1]}`)
         process.exit(1)
       }
     })
