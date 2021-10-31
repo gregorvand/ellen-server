cf33211c47b85aa9fc1dc9f1f088ebe804584b15
Gregor Vand <gregor@pixelcabin.io>
2021-10-31 13:34:35 +0800

    update DB snippets ref

--- DB_snippets.sql
+++ DB_snippets.sql
@@ -10,3 +10,20 @@ where
 order by
   "orderNumber",
   "emailDate"
+-- finds unique order numbers, only showing the earlist record --
+-- with date filtering
+-- also returns results in format of orderNumber / emailDate columns only, with renamed columns
+-- And filters on only order numbers that are 'whole numbers' ie no '-' or letters
+select
+  distinct on ("orderNumber") "orderNumber" "y",
+  "emailDate" "t"
+from
+  public."EdisonOrders"
+where
+  "fromDomain" = '${req.body.companyEmail}'
+  and "orderNumber" ~ '^\\d+$'
+  and "emailDate" between '${req.body.dateStart}' :: timestamp
+  and '${req.body.dateEnd}' :: timestamp
+order by
+  "orderNumber",
+  "emailDate"
