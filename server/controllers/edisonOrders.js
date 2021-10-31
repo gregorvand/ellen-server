bc108e00ee9a93124dc1332cbabe4e4c3465b41c
Gregor Vand <gregor@pixelcabin.io>
2021-10-31 13:32:31 +0800

    working lookup of unique order numbers (earliest only) between two dates

--- server/controllers/edisonOrders.js
+++ server/controllers/edisonOrders.js
@@ -1,6 +1,10 @@
+// Controller related to the EdisonOrder model
+// currently we also have 'ordersByEdison' controller which was for Order model
+
 const EdisonOrder = require('../models').EdisonOrder
 const Company = require('../models').Company
 const db = require('../models/index')
+const { Op } = require('sequelize')
 
 const insertEdisonRowNoId = async function (...edisonRow) {
   const edisonData = edisonRow[0]
@@ -29,14 +33,49 @@ const insertEdisonRowNoId = async function (...edisonRow) {
   }
 }
 
-const edisonOrdersUnqiueOrderNumber = async function (req, res) {
+const edisonOrdersUniqueOrderNumber = async function (req, res) {
+  // const [results] = await db.sequelize.query(
+  //   `select distinct on ("orderNumber") *
+  //   from public."EdisonOrders"
+  //   where "fromDomain" = '${req.body.companyEmail}'
+  //   order by "orderNumber", "emailDate"`
+  // )
+  // res.send(results).status(200)
+
+  console.log(`${req.body.dateStart.toString()}`)
+
   const [results] = await db.sequelize.query(
-    `select distinct on ("orderNumber") *
+    `select distinct on ("orderNumber")
+    "orderNumber" "y",
+    "emailDate" "t"
     from public."EdisonOrders"
-    where "fromDomain" = '${req.body.companyEmail}' 
+    where "fromDomain" = '${req.body.companyEmail}'
+    and "orderNumber" ~ '^\\d+$'
+    and "emailDate" between '${req.body.dateStart}'::timestamp and '${req.body.dateEnd}'::timestamp
     order by "orderNumber", "emailDate"`
   )
   res.send(results).status(200)
+
+  // and "emailDate" between ${toString(
+  //   dayjs(req.body.dateStart).format('YYYY-MM-DD')
+  // )} and ${toString(dayjs(req.body.dateEnd).format('YYYY-MM-DD'))}
+
+  // const company = await Company.findOne({ where: { id: req.body.id } })
+  // return EdisonOrder.findAll({
+  //   where: {
+  //     fromDomain: company.emailIdentifier,
+  //     orderNumber: {
+  //       [Op.regexp]: '^\\d+$',
+  //     },
+  //     emailDate: {
+  //       [Op.between]: [req.body.dateStart, req.body.dateEnd], // default for all dates
+  //     },
+  //   },
+  //   order: ['orderNumber', 'id'],
+  //   // order: [['emailDate', 'ASC']],
+  // })
+  //   .then((orders) => res.send(orders))
+  //   .catch((error) => console.error('error with company page lookup', error))
 }
 
 // pass in company, year
@@ -44,26 +83,26 @@ const edisonOrdersUnqiueOrderNumber = async function (req, res) {
 // constructed with help via https://stackoverflow.com/questions/69127003/mixing-distinct-with-group-by-postgres
 const monthsAvailableByYear = async function (req, res) {
   const company = await Company.findOne({ where: { id: req.body.companyId } })
-
+  console.log(company.emailIdentifier)
   const [results] = await db.sequelize.query(
     `SELECT
-          DATE_PART('month', "orderDate") AS month,
-          COUNT(DISTINCT "orderDate"::date) AS count
-      FROM "Orders"
-      WHERE
-        "fromDomain" = '${company.emailIdentifier}' AND
-        "orderNumber" != 1 AND
-        DATE_PART('year', "orderDate") = ${req.body.year}
-      GROUP BY
-        DATE_PART('month', "orderDate")
-      HAVING
-        COUNT(DISTINCT "orderDate"::date) > 1;`
+        DATE_PART('month', "emailDate") AS month,
+        COUNT(DISTINCT "emailDate"::date) AS count
+    FROM "EdisonOrders"
+    WHERE
+      "orderNumber" ~ '^\\d+$' AND
+      "fromDomain" = '${company.emailIdentifier}' AND
+      DATE_PART('year', "emailDate") = '${req.body.year}'
+    GROUP BY
+      DATE_PART('month', "emailDate")
+    HAVING
+      COUNT(DISTINCT DATE_PART('day', "emailDate")) > 2;`
   )
   res.send(results).status(200)
 }
 
 module.exports = {
   insertEdisonRowNoId: insertEdisonRowNoId,
-  edisonOrdersUnqiueOrderNumber: edisonOrdersUnqiueOrderNumber,
+  edisonOrdersUniqueOrderNumber: edisonOrdersUniqueOrderNumber,
   monthsAvailableByYear: monthsAvailableByYear,
 }
