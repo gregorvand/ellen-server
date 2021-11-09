a6c9fb46e0ac61e123b3b79a03694300da8ce519
Gregor Vand <gregor@pixelcabin.io>
2021-11-09 08:44:30 +0800

    Adds migration for adding unique emailIdentifier index to Company model

--- server/models/company.js
+++ server/models/company.js
@@ -1,45 +1,57 @@
 module.exports = (sequelize, DataTypes) => {
-  const Company = sequelize.define('Company', {
-    nameIdentifier: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    emailIdentifier: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    orderPrefix: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    orderSuffix: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    companyType: {
-      type: DataTypes.STRING,
-      validate: {
-        isIn: [['private', 'public']],
+  const Company = sequelize.define(
+    'Company',
+    {
+      nameIdentifier: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
-      allowNull: true,
+      emailIdentifier: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      orderPrefix: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      orderSuffix: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      companyType: {
+        type: DataTypes.STRING,
+        validate: {
+          isIn: [['private', 'public']],
+        },
+        allowNull: true,
+      },
+      ticker: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      sector: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      industry: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      exchangeShortName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
-    ticker: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    sector: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    industry: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    exchangeShortName: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-  })
+    {
+      indexes: [
+        // Create a unique index on email
+        {
+          unique: true,
+          fields: ['emailIdentifier'],
+        },
+      ],
+    }
+  )
 
   Company.associate = (models) => {
     Company.hasMany(models.Earning, {
