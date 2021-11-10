8100e1c45c01da536e345bc70f07d5396e6319f1
Gregor Vand <gregor@pixelcabin.io>
2021-11-10 13:25:30 +0800

    document helper

--- server/utils/generateCompanyRegex.js
+++ server/utils/generateCompanyRegex.js
@@ -1,3 +1,5 @@
+// Returns a regex that matches a company's orderPrefix if
+// the company has one assigned other than '#' (default)
 function generateCompanyRegex(orderPrefix) {
   let regex = `^\\d+$`
   if (orderPrefix !== '#') {
